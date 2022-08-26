cd17e41049dc86bb40a497290b5308f8ed044aeb
root <root@cad576bdf91c.mylabserver.com>
2022-08-26 09:12:21 +0000

    Refactored to use Hiera

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,11 +1,8 @@
 # @summary Apache Install Base Package
 #
 # 
-class apache::install (
-  $install_name   = $apache::params::install_name,
-  $install_ensure = $apache::params::install_ensure,
-) inherits apache::params {
- package { "${install_name}":
-  ensure => "${install_ensure}",
+class apache::install {
+ package { "${apache::install_name}":
+  ensure => "${apache::install_ensure}",
  }  
 }
